27e26409813b191d25d40e056a20f1ca24f77f3f
Rosenrot3585 <mcmp3585@gmail.com>
2023-10-02 20:36:56 -0300

    one more

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("A commit has been made.");
         System.out.println("Another commit has been made.");
+        System.out.println("One more commit has been made.");
     }
 }
